dacec60b56b1fad626126bc2a07d8d095d0b0f6e
snakajim <snakajim@arm.com>
2021-04-29 20:35:38 +0900

    some fixed

--- scripts/install_beast_mode.sh
+++ scripts/install_beast_mode.sh
@@ -17,7 +17,6 @@ fi
 
 if [ $# = 0 ] || [ $1 == "beast" ]; then
   echo "Setting beast mode."
-  sudo /usr/bin/jetson_clocks --show
   cd /sys/devices/gpu.0/devfreq/17000000.gv11b
   cat available_frequencies
   sudo sh -c "echo 114750000 > max_freq"
@@ -25,6 +24,8 @@ if [ $# = 0 ] || [ $1 == "beast" ]; then
   cat available_frequencies
   sudo sh -c "echo 114750000 > max_freq"
   sudo /usr/bin/jetson_clocks
+  echo "Your setting is now"
+  sudo /usr/bin/jetson_clocks --show
   sudo /usr/sbin/nvpmodel -d cool
   sudo /usr/sbin/nvpmodel -q
 else
@@ -35,7 +36,8 @@ else
     sudo sh -c "echo 1109250000 > max_freq"
     cd /sys/devices/17000000.gv11b/devfreq/17000000.gv11b
     sudo sh -c "echo 1109250000 > max_freq"
-    sudo /usr/bin/jetson_clocks.sh --show
+    echo "Your setting is now"
+    sudo /usr/bin/jetson_clocks --show
   else
     echo "Bad argument."
     echo "Arg should be either beast or default, but $1."
